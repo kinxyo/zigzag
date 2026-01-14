95a1d0d1ba67c188a4bf6521232ef0da3f20cae7
kinxyo <runner.brassy_0l@icloud.com>
2026-01-14 22:44:51 +0530

    tui init

--- src/tui.zig
+++ src/tui.zig
@@ -1,1 +1,19 @@
-pub fn run() !void {}
+const std = @import("std");
+const tui = @import("tuilip");
+
+pub fn run() !void {
+    var gpa: std.heap.GeneralPurposeAllocator(.{}) = .{};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var cv: tui.Canvas = try .init(allocator);
+    defer cv.deinit();
+
+    const text: tui.Text = .{ .value = "Working in progress..." };
+
+    // TODO: add enums for position for tuilip.
+    try cv.renderCS(text, .{ .col = @intCast(cv.getCol() - text.value.len), .row = cv.getRow() - 1 }, .draw);
+    cv.flush();
+
+    std.Thread.sleep(std.time.ns_per_s * 5);
+}
